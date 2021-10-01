e94491018935116847ace7135f1c781f3f54e533
Junhyuk <62590913+deeptwinkle@users.noreply.github.com>
2021-10-01 16:23:04 +0900

    Create 위장.py

---
+++ 프로그래머스/해시/위장.py
@@ -0,0 +1,14 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    
+    for i in clothes:
+        if i[1] not in dic:
+            dic[i[1]] = 1
+        else:
+            dic[i[1]] += 1
+    
+    for i in dic.values():
+        answer *= (i + 1)
+        
+    return answer - 1
