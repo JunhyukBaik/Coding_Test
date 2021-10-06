affea669665b01c791bae5e8ffdd4b4381f5dc9c
Junhyuk <62590913+deeptwinkle@users.noreply.github.com>
2021-10-07 00:11:25 +0900

    Update 크레인 인형뽑기 게임.py

--- 프로그래머스/크레인 인형뽑기 게임.py
+++ 프로그래머스/크레인 인형뽑기 게임.py
@@ -1,17 +1,19 @@
 def solution(board, moves):
     answer = 0
-    cnt = 0
-
+    stack = []
+    
     for j in moves:
         for i in range(len(board)):
             if board[i][j-1] == 0:
                 continue
             else:
-                if board[i][j-1] != cnt:
-                    cnt = board[i][j-1]
-                else:
-                    answer += 2
-                    cnt = 0
+                stack.append(board[i][j-1])
                 board[i][j-1] = 0
+                
+                if len(stack) > 1:
+                    if stack[-1] == stack[-2]:
+                        del stack[-1]
+                        del stack[-1]
+                        answer += 2                
                 break
     return answer
